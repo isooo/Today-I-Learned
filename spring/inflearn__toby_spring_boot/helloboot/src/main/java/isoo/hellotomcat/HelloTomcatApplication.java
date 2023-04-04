d74179fc39fb70f3fd6fb31efc5bab791d93850d
isooo <midnightmarching@gmail.com>
2023-04-05 00:16:56 +0900

    feat:#60: spring: 토비의 스프링 부트: (ch4) DispatcherServlet으로 전환 및 애너테이션 매핑 정보 사용
    - spring6부터는 DispatcherServlet이 타입레벨에 단독으로 존재하는 `@RequestMapping`을 인식하지 못함. 하여 `@Controller`도 함께 지정해줘야 한다
    - ViewHandler 역할을 할 수 있도록 `@ResponseBody` 추가

--- spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/hellotomcat/HelloTomcatApplication.java
+++ spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/hellotomcat/HelloTomcatApplication.java
@@ -1,57 +1,31 @@
 package isoo.hellotomcat;
 
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-
-import java.io.IOException;
+import org.springframework.web.context.support.GenericWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class HelloTomcatApplication {
     public static void main(String[] args) {
+        // 스프링 컨테이너 만들기
+        final GenericWebApplicationContext applicationContext = new GenericWebApplicationContext();
+        applicationContext.registerBean(HelloController.class); // 컨트롤러를 직접 생성해줘도 되지만, 클래스로 등록해줄 수 있다
+        applicationContext.registerBean(SimpleHelloService.class); // 인터페이스 타입으로 클래스를 생성할 수 없으니, 구현체를 등록
+        applicationContext.refresh(); // 등록된 정보들을 바탕으로 컨테이너를 초기화 함(bean 오브젝트들을 만듦)
+
         // 부트에 내장된 톰캣 서블릿 컨테이너를 만들 수 있는 팩토리 클래스
         final ServletWebServerFactory serverFactory = new TomcatServletWebServerFactory();
         // 필요에 따라 jetty, undertow 등 다른 서버로 교체할 수 있도록,
         // 각 서버팩토리는 WebServerFactory를 구현하고 있다
 
-        final GenericApplicationContext applicationContext = new GenericApplicationContext();
-        applicationContext.registerBean(HelloController.class); // 컨트롤러를 직접 생성해줘도 되지만, 클래스로 등록해줄 수 있다
-        applicationContext.registerBean(SimpleHelloService.class); // 인터페이스 타입으로 클래스를 생성할 수 없으니, 구현체를 등록
-        applicationContext.refresh(); // 등록된 정보들을 바탕으로 컨테이너를 초기화 함(bean 오브젝트들을 만듦)
-
-        final HelloController helloController = applicationContext.getBean(HelloController.class);// 타입을 통해, 등록되 bean 오브젝트를 가져옴
-        final WebServer webServer = serverFactory.getWebServer(servletContext -> addFrontController(servletContext, helloController));
-        webServer.start();
-    }
-
-    // 인증, 보안, 다국어, 공통 기능을 처리할 프론트 컨트롤러
-    private static void addFrontController(ServletContext servletContext, HelloController helloController) {
-                                                            // 서블릿 컨테이너에 서블릿을 등록하기 위해 필요한 작업을 해줄 오브젝트를
-                                                            // 파라미터로 전달한다
-        servletContext.addServlet("FrontController", new HttpServlet() {
-            @Override
-            protected void service(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-
-                if ("/hello".equals(req.getRequestURI()) && HttpMethod.GET.name().equals(req.getMethod())) {
-                    final String name = req.getParameter("name"); // 서블릿 요청을 이용해보자
-
-                    final String body = helloController.hello(name);
-
-                    // 응답 만들기
-                    resp.setContentType(MediaType.TEXT_PLAIN_VALUE); // Content-Type: text/plain
-                    resp.getWriter().println(body);
-                } else {
-                    resp.setStatus(HttpStatus.NOT_FOUND.value());
+        final WebServer webServer = serverFactory.getWebServer(
+                servletContext -> {
+                    servletContext.addServlet(
+                            "dispatcherServlet", new DispatcherServlet(applicationContext)
+                    ).addMapping("/*"); // 모든 요청을 이 서블릿이 받을 수 있게!
                 }
-            }
-        }).addMapping("/*"); // 모든 요청을 이 서블릿이 받을 수 있게!
+        );
+        webServer.start();
     }
 }
