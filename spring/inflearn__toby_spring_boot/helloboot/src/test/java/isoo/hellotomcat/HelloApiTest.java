fae9e01bc3dec9bb26430b288dc4c433ff1718c6
isooo <midnightmarching@gmail.com>
2023-04-10 20:11:42 +0900

    feat:#60: spring: 토비의 스프링 부트: (ch5) TestRestTemplate을 이용해 api를 테스트해보자

---
+++ spring/inflearn__toby_spring_boot/helloboot/src/test/java/isoo/hellotomcat/HelloApiTest.java
@@ -0,0 +1,23 @@
+package isoo.hellotomcat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HelloApiTest {
+    @Test
+    void 웹_응답의_3가지_요소를_검증한다() {
+        final TestRestTemplate testRestTemplate = new TestRestTemplate();
+        final ResponseEntity<String> responseEntity =
+                testRestTemplate.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+
+        assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+        assertThat(responseEntity.getHeaders().getFirst(HttpHeaders.CONTENT_TYPE)).startsWith(MediaType.TEXT_PLAIN_VALUE);
+        assertThat(responseEntity.getBody()).isEqualTo("Hello Spring");
+    }
+}
