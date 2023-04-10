24a7f5b0e36e62c9370fd27a4bc8e1dcd8d27713
isooo <midnightmarching@gmail.com>
2023-04-10 17:54:32 +0900

    feat:#60: spring: 토비의 스프링 부트: (ch4) `@Component`를 이용해 bean을 등록하자
    - 이렇게하면 편리하긴 하지만, 어떤 bean이 등록되었는지 한 눈에 보기 어렵다는 단점이 있다

--- spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/hellotomcat/HelloTomcatApplication.java
+++ spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/hellotomcat/HelloTomcatApplication.java
@@ -3,23 +3,14 @@ package isoo.hellotomcat;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
 @Configuration
+@ComponentScan
 public class HelloTomcatApplication {
-
-    @Bean
-    public HelloController helloController(final HelloService helloService) {
-        return new HelloController(helloService);
-    }
-
-    @Bean
-    public HelloService helloService() {
-        return new SimpleHelloService();
-    }
 
     public static void main(String[] args) {
         // 스프링 컨테이너 만들기
--- spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/hellotomcat/SimpleHelloService.java
+++ spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/hellotomcat/SimpleHelloService.java
@@ -1,5 +1,8 @@
 package isoo.hellotomcat;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class SimpleHelloService implements HelloService {
     @Override
     public String sayHello(final String name) {
