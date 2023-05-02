6d8c97a9fc65d42092ba8a70577d865a79806bce
isooo <midnightmarching@gmail.com>
2023-05-02 22:17:16 +0900

    feat:#60: spring: 토비의 스프링 부트: (ch6) 외부 파일을 이용하여 구성 정보를 등록해보자
    - `@MyAutoConfiguration` 애너테이션을 선언하고, 이게 붙은 타입은 구성정보로 사용하기 위해 `@Configuration`을 붙여준다
    - `MyAutoConfigImportSelector`에 `@MyAutoConfiguration` 애너테이션을 등록해준다
        - 이 애너테이션에 의해 등록된 녀석들을 설정정보로 사용하겠다고 등록하기 위함
        - `ImportCandidates.load`에, 어떤 형식으로 파일을 저장해야된다고 기술되어 있다
            - 클래스패스에 `META-INF/spring/full-qualified-annotation-name.import` 형태의 파일을 읽어온다!
    - `isoo.config.MyAutoConfiguration.imports` 파일에, 구성정보로 사용할 클래스를 등록해준다
    - ==> 선언한 애너테이션과 동일한 이름의 파일에서, configuration 파일 목록을 가져오고, 이 파일들이 구성 정보로 import 되도록 만들었다

--- spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/config/MyAutoConfigImportSelector.java
+++ spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/config/MyAutoConfigImportSelector.java
@@ -1,14 +1,26 @@
 package isoo.config;
 
+import org.springframework.boot.context.annotation.ImportCandidates;
 import org.springframework.context.annotation.DeferredImportSelector;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MyAutoConfigImportSelector implements DeferredImportSelector {
+    private final ClassLoader classLoader;
+
+    public MyAutoConfigImportSelector(final ClassLoader classLoader) {
+        this.classLoader = classLoader;
+    }
+
     @Override
     public String[] selectImports(final AnnotationMetadata importingClassMetadata) {
-        return new String[]{
-                "isoo.config.autoconfig.DispatcherServletConfig",
-                "isoo.config.autoconfig.TomcatWebServerConfig"
-        };
+        final List<String> autoConfigs = new ArrayList<>();
+
+        ImportCandidates.load(MyAutoConfiguration.class, classLoader)
+                .forEach(autoConfigs::add);
+
+        return autoConfigs.toArray(String[]::new);
     }
 }
