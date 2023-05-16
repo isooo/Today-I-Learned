c020a03cb2a1ec6e3827d9d0c258f31eba368a03
isooo <midnightmarching@gmail.com>
2023-05-16 09:54:46 +0900

    feat:#60: spring: 토비의 스프링 부트: (ch7) 조건에 따라 생성되는 bean이 달라지도록 해보자: `@ConditionalOnMissingBean` 사용해보기
    - 사용자가 만든 구성 정보와, 부트가 제공하는 구성 정보(자동구성정보) 가 동일한 타입을 bean으로 등록하고자 할 때, `@ConditionalOnMissingBean`를 이용하면 해당 애너테이션에 붙은 메서드/클래스는 동일 타입이 bean으로 등록되지 않았을 때 bean으로 등록시켜 준다

--- spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/config/autoconfig/JettyWebServerConfig.java
+++ spring/inflearn__toby_spring_boot/helloboot/src/main/java/isoo/config/autoconfig/JettyWebServerConfig.java
@@ -2,6 +2,7 @@ package isoo.config.autoconfig;
 
 import isoo.config.ConditionalMyOnClass;
 import isoo.config.MyAutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 @ConditionalMyOnClass("org.eclipse.jetty.server.Server")
 public class JettyWebServerConfig {
     @Bean("jettyServletWebServerFactory")
+    @ConditionalOnMissingBean
     public ServletWebServerFactory servletWebServerFactory() {
         return new JettyServletWebServerFactory();
     }
